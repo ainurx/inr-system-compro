e9ed1470dbe1d2b3e3e69d09ef21238bb8842bd6
ainurx <ainurrahmat1@gmail.com>
2020-06-16 02:09:00 +0700

    Base Homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {BrowserRouter as Router,
+        Link, Switch, Route} from 'react-router-dom'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from'./components/Home'
+import About from'./components/About'
+
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link className="navbar-brand" to="/">INREADY WORKGROUP</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="">Kegiatan</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="">Artikel</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="">Karya</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About}/>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import '../App.css'
+
+class Home extends Component{
+  render(){
+    return(
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src="1.jpg" alt="First slide"/>
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="2.jpg" alt="Second slide"/>
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="3.jpg" alt="Third slide"/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
